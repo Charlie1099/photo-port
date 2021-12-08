fc6e83bb4dbc864f5c65dd6ba3d9313c4f03e1f0
charlie1099 <cdcharlesworth@gmail.com>
2021-12-07 20:44:10 -0700

    added contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import Nav from "./components/Nav"
+import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-
+import ContactForm from './components/Contact';
 
 function App() {
-
   const [categories] = useState([
     {
       name: 'commercial',
@@ -15,8 +14,10 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
-  
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -24,14 +25,20 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        >
-      </Nav>
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-      <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
 }
-
 export default App;
